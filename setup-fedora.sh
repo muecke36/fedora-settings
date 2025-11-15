0bff13408c46d9ba69b2500745802a364ab34d7d
muecke36 <michael@muecken.me>
2025-11-15 20:53:10 +0100

    Install Azure DevOps extension for Azure CLI
    Add Azure DevOps extension to Azure CLI installation.

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -10,6 +10,8 @@ install_basics() {
   sudo dnf install -y https://packages.microsoft.com/config/rhel/9.0/packages-microsoft-prod.rpm
   sudo dnf install azure-cli
 
+  az extension add --name azure-devops
+
   dnf check-update
 
   # Basics
